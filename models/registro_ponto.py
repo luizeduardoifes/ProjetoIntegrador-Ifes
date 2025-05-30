ad3a83f0f375c71a383b8756d3d9eb39f7064786
Luiz Eduardo Lima Balbino <luizeduardo071205@gmail.com>
2025-05-29 21:40:18 -0300

    remove unused registro_ponto functions and update SQL statements for registro_ponto table

--- models/registro_ponto.py
+++ models/registro_ponto.py
@@ -2,19 +2,12 @@
 from dataclasses import dataclass
 from datetime import date, time
 
-from sqlalchemy import ForeignKey
-
-from models.remetente import Remetente
-
 
 @dataclass
-class RegistroPonto(Remetente):
+class RegistroPonto:
     id: int
-    remetente: Remetente = ForeignKey('Remetente.id', ondelete='CASCADE')
     data: date
     entrada: time
     entrada_intervalo: time
     saida_intervalo: time
     saida: time
-    horas_trabalhadas: float
-    
--- sql/registro_ponto_sql.py
+++ sql/registro_ponto_sql.py
@@ -1,13 +1,40 @@
 CREATE_TABLE_REGISTRO_PONTO = """
-CREATE TABLE IF NOT EXISTS RegistroPonto (
+CREATE TABLE IF NOT EXISTS registro_ponto (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    remetente TEXT NOT NULL,
     data DATE NOT NULL,
+    remetente TEXT NOT NULL,
     entrada TIME NOT NULL,
     entrada_intervalo TIME NOT NULL,
     saida_intervalo TIME NOT NULL,
-    saida TIME NOT NULL,
-    horas_trabalhadas REAL NOT NULL
+    saida TIME NOT NULL
 );
 """
 
+INSERT_REGISTRO_PONTO = """
+INSERT INTO registro_ponto (data, remetente, entrada, entrada_intervalo, saida_intervalo, saida)
+VALUES (?, ?, ?, ?, ?, ?);
+"""
+
+UPDATE_REGISTRO_PONTO = """
+UPDATE registro_ponto
+SET data = ? remetente = ? entrada = ?, entrada_intervalo = ?, saida_intervalo = ?, saida = ?
+WHERE id = ?;
+"""
+
+DELETE_REGISTRO_PONTO = """
+DELETE FROM registro_ponto
+WHERE id = ?;
+"""
+
+GET_REGISTRO_PONTO_BY_ID = """
+SELECT id, data, remetente, entrada, entrada_intervalo, saida_intervalo, saida
+FROM registro_ponto
+WHERE id = ?;
+"""
+
+GET_REGISTRO_PONTO_BY_PAGE = """
+SELECT id, data, remetente, entrada, entrada_intervalo, saida_intervalo, saida
+FROM registro_ponto
+ORDER BY data DESC
+LIMIT ? OFFSET ?;
+"""
