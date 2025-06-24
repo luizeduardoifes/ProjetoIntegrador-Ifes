a7face70a73b65047a0cd39a124997b32b7597ab
Luiz Eduardo Lima Balbino <luizeduardo071205@gmail.com>
2025-06-24 19:36:41 -0300

    atualiza inserts de remetente e altera a tabela de Produto para Encarregado

--- data/inserts_remetente.sql
+++ data/inserts_remetente.sql
@@ -1,2 +1,5 @@
-INSERT INTO Produto (usuario, senha) VALUES (1, "luizeduardo", "123456");
-INSERT INTO Produto (usuario, senha) VALUES (2, "igorgomes", "654321")
+-- database: ../dados.db
+
+INSERT INTO Encarregado (usuario, senha) VALUES ("luizeduardo", "123456");
+INSERT INTO Encarregado (usuario, senha) VALUES ("igorgomes", "654321");
+
